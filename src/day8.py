32c11861bd3363b6b268a5dd187f5442742a22be
Amalia Dann <125802798+Amalia-Dann@users.noreply.github.com>
2023-12-11 23:09:14 +0100

    Create day8.py

---
+++ src/day8.py
@@ -0,0 +1,7 @@
+print("Ich rechne deinen Notenschnitt für dich aus!")
+mathe = float(input("Mathenote: "))
+deutsch = float(input("Deutschnote: "))
+englisch = float(input("Englischnote: "))
+
+durchschnitt = (mathe + deutsch + englisch)/3
+print(f"Der Durchschschnitt deiner Hauptfächer ist: {durchschnitt}")

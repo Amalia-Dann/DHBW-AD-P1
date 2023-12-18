2972889fc0873496c4104bbb4d763d28983612d9
Amalia Dann <125802798+Amalia-Dann@users.noreply.github.com>
2023-12-18 23:37:06 +0100

    Create day15.py

---
+++ src/day15.py
@@ -0,0 +1,5 @@
+laenge = 5
+for i in range (laenge):
+    print(i * "*",  (laenge-i)* " ", (laenge-i)* " ", i * "+" )
+for i in range (laenge, 0, -1):
+    print(i * "*", (laenge-i)* " ", (laenge-i)* " ", i * "+" )

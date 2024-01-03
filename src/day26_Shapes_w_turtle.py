02c22102134ed96a40ee3b5f1a5cd3bd7cbc1e92
Amalia Dann <125802798+Amalia-Dann@users.noreply.github.com>
2024-01-03 21:20:43 +0100

    Create day26_Shapes_w_turtle.py

---
+++ src/day26_Shapes_w_turtle.py
@@ -0,0 +1,45 @@
+import turtle
+turtle.showturtle()
+turtle.shape("turtle")
+turtle.pencolor('red')
+for x in range(50):
+        turtle.forward(30)
+        if x % 2 == 0:
+            turtle.left(40)
+        else:
+            turtle.left(60)
+turtle.pencolor('orange')
+for x in range(50):
+        turtle.forward(50)
+        if x % 2 == 0:
+            turtle.left(75)
+        else:
+            turtle.left(100)
+turtle.pencolor('yellow')
+for x in range(50):
+        turtle.forward(80)
+        if x % 2 == 0:
+            turtle.left(110)
+        else:
+            turtle.left(135)
+turtle.pencolor('green')
+for x in range(50):
+        turtle.forward(100)
+        if x % 2 == 0:
+            turtle.left(145)
+        else:
+            turtle.left(170)
+turtle.pencolor('blue')
+for x in range(50):
+        turtle.forward(130)
+        if x % 2 == 0:
+            turtle.left(180)
+        else:
+            turtle.left(205)
+turtle.pencolor('purple')
+for x in range(50):
+        turtle.forward(150)
+        if x % 2 == 0:
+            turtle.left(115)
+        else:
+            turtle.left(140)

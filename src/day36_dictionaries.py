dd1eabca1be10e990cd24f31b68d475ed27d193b
Amalia Dann <125802798+Amalia-Dann@users.noreply.github.com>
2024-01-20 15:06:28 +0100

    Create day36_dictionaries.py
    Tried out some Code from: 
    https://www.uni-regensburg.de/assets/physik/fakultaet/IT/Tutorials-Installation-Programming-Environment/Programmieren_in_Python.pdf

---
+++ src/day36_dictionaries.py
@@ -0,0 +1,9 @@
+l_keys1 = list( houseStark )
+l_keys2 = list( houseStark.keys() )
+l_values = list( houseStark.values() )
+l_items = list( houseStark.items() )
+
+print("direct conversion: ", l_keys1 )
+print("method keys() : ", l_keys2 )
+print("method values() : ", l_values)
+print("method items() : ", l_items )
